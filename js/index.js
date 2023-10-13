5d6bf0e0f1761cca7f3cd38f069c24f380d8d7c4
Taejin Kim <rlaxowls1316@naver.com>
2023-10-13 17:08:16 +0900

    :sparkles: Clouds 움직이기 구현

--- js/index.js
+++ js/index.js
@@ -4,6 +4,8 @@ const ctx = canvas.getContext("2d");
 canvas.width = 1424;
 canvas.height = 800;
 
+const FIELD_LENGTH = 30000;
+
 class Player {
   constructor({ position }) {
     this.position = position;
@@ -21,16 +23,25 @@ class Cloud {
   constructor({ position, radius }) {
     this.position = position;
     this.radius = radius;
+    this.velocity = {
+      x: 0,
+      y: 5,
+    };
   }
 
   draw() {
     ctx.beginPath();
-    ctx.globalAlpha = "0.3";
+    ctx.globalAlpha = "0.7";
     ctx.fillStyle = "white";
     ctx.arc(this.position.x, this.position.y, this.radius, 0, 2 * Math.PI);
     ctx.fill();
     ctx.closePath();
     // ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  update() {
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
   }
 }
 
@@ -43,12 +54,16 @@ const player = new Player({
 
 const clouds = [];
 
-for (let i = 0; i < 20; i++) {
+for (let i = 0; i < 400; i++) {
   clouds.push(
     new Cloud({
       position: {
-        x: Math.floor(Math.random() * canvas.width) + 0,
-        y: Math.floor(Math.random() * canvas.height) + 0,
+        x:
+          Math.floor(Math.random() * (canvas.width + FIELD_LENGTH)) -
+          FIELD_LENGTH,
+        y:
+          Math.floor(Math.random() * (canvas.height + FIELD_LENGTH)) -
+          FIELD_LENGTH,
       },
       radius: Math.floor(Math.random() * 80) + 50,
     })
@@ -61,6 +76,7 @@ function animate() {
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   for (let i = 0; i < clouds.length; i++) {
+    clouds[i].update();
     clouds[i].draw();
   }
 
